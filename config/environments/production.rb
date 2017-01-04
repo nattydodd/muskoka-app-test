22eb2225216c575fc8b32f6728dbad9ab4efe195
Natalie Dodd <nataliedodd89@gmail.com>
2017-01-04 00:19:16 -0500

    tried added cloudinary config cloud name to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  Cloudinary.config.cloud_name = dx3z90dgl
   # Code is not reloaded between requests.
   config.cache_classes = true
 
