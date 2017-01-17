2d850863d7e5ac7b60d145d4669ab9f5074a7a06
Natalie Dodd <nataliedodd89@gmail.com>
2017-01-17 13:45:13 -0500

    changed the navbar and toggle active state

---
+++ app/assets/javascripts/navbar.js
@@ -0,0 +1,22 @@
+
+  //
+  // $(window).bind('beforeunload', function(){
+  //     $('#navbar li').each(function(){
+  //         $(this).removeClass("active");
+  //     });
+  // });
+
+
+  //adds active state to the current page on load
+  $( document ).on('turbolinks:load', function() {
+      var currentPage = $(location).attr('pathname').replace('/', '');
+      $('#navbar li').each(function () {
+          var itemId = $(this).attr('id');
+
+          //we check the currentPage matches the Id
+          if (itemId.indexOf(currentPage) >= 0) {
+              $(this).addClass("active");
+              return false;
+          }
+      });
+  });
